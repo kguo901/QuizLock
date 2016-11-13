478cd80501545271ba31a6848b38c814860aa132
Kathy Guo <guo.901@osu.edu>
2016-11-12 22:40:26 -0500

    Added Lockscreen activity

--- app/src/main/java/com/example/guo/quizlock/MainActivity.java
+++ app/src/main/java/com/example/guo/quizlock/MainActivity.java
@@ -3,12 +3,11 @@ package com.example.guo.quizlock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +21,7 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, AddSetActivity.class);
+                Intent intent = new Intent(MainActivity.this, AddSetActivity.class);
                 startActivity(intent);
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                        // .setAction("Action", null).show();
